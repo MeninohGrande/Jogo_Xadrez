f1cac6877adcb178aab1c7e66369bb6db5a0ca29
MeninohGrande <pedro.armison@outlook.com>
2021-04-08 23:03:39 -0300

    Implementado novas peças do jogo

---
+++ src/chess/pieces/Knight.java
@@ -0,0 +1,26 @@
+package chess.pieces;
+
+import board.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Knight extends ChessPiece{
+
+    public Knight(Board board, Color color) {
+        super(board, color);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
